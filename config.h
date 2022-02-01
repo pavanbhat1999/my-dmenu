7028a809f74e08a7041bd2713573b8db39bcd67b
pavanbhat1999 <prbhat07@gmail.com>
2022-02-01 10:20:21 +0530

    Fuzzy searching successfully added

--- config.h
+++ config.h
@@ -5,14 +5,14 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"FiraCode:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#eee8d5", "#002b36" },
+    [SchemeSel] = { "#eee8d5", "#b58900" },
+    [SchemeOut] = { "#000000", "#00FFFF" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
